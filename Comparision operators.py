6aabffbdc5e24a87ad6517effe08bfe56c45e0fe
sornanagaraj <sorna23@gmail.com>
2023-12-28 13:59:05 +0000

    Testing

---
+++ Comparision operators.py
@@ -0,0 +1,21 @@
+a = 9
+b = 5
+  
+# Output 
+#Greater than: True if the left operand is greater than the right
+print(a > b)
+
+#Less than: True if the left operand is less than the right
+print(a < b)
+
+#Equal to: True if both operands are equal
+print(a == b)
+
+#Not equal to – True if operands are not equal
+print(a != b)
+
+#Greater than or equal to: True if left operand is greater than or equal to the right
+print(a >= b)
+
+#Less than or equal to: True if left operand is less than or equal to the right
+print(a <= b)
---
+++ Datatypes.py
@@ -0,0 +1,40 @@
+# Determining Types
+#Integer
+a = 5
+print("Type of a: ", type(a))
+
+#Float
+b = 5.0
+print("\nType of b: ", type(b))
+
+#Complex
+c = 2 + 4j
+print("\nType of c: ", type(c))
+
+#None
+d = None
+print("\nType of e: ", type(d)) 
+
+# String 
+e = 'Infrastructure as Code'
+print("\nType of d: ", type(e)) 
+  
+#Boolean
+f = True
+print("\nType of d: ", type(f)) 
+
+
+#Dynamic Typing
+# This will store 6 in the memory and binds the 
+# name x to it. After it runs, type of x will 
+# be int. 
+x = 5 
+  
+print(type(x)) 
+  
+# This will store 'hello' at some location int  
+# the memory and binds name x to it. After it 
+# runs type of x will be str. 
+x = 'hello' 
+  
+print(type(x)) 
